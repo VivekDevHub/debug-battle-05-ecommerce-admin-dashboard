97b972c60415fe23a0486f1284053e506cec6cd6
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-06-07 00:06:04 +0530

    fixing

--- server/middleware/errorMiddleware.js
+++ server/middleware/errorMiddleware.js
@@ -1,9 +1,9 @@
 const errorHandler = (err, req, res, next) => {
-  const statusCode = res.statusCode ? res.statusCode : 500;
+  const statusCode = (res.statusCode && res.statusCode >= 400) ? res.statusCode : 500;
   res.status(statusCode);
   res.json({
-    message: "Request processing error. Check backend console.",
-    stack: null,
+    message: err.message || "Request processing error.",
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
   });
 };
 
