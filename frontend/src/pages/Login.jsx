af077781abf5691ddc45b81a86b66382cbe1b3b3
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-06-06 22:49:42 +0530

    bugs input name="pass" give full name password

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -68,7 +68,7 @@ const Login = () => {
               <Input
                 id="password"
                 type="password"
-                name="pass"
+                name="password" // name should be password for sending data correctly
                 value={formData.password}
                 onChange={handleChange}
                 required
